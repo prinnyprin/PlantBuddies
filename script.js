58aa0fc9471339aa1fc39278d3583b3d36ad0dae
Carrima Hewitt <milishania@gmail.com>
2023-07-10 14:00:03 -0400

    Update UI

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let dropInBtn = document.querySelector(".dropIn-btn");
+///SIDEBAR//
+let sidebarItems = document.querySelectorAll(".sidebar-item");
+sidebarItems.forEach((item) =>{
+    item.addEventListener("click", ()=>{
+        let curr = document.querySelector(".side-curr");
+        curr.classList.remove("side-curr");
+        item.classList.add("side-curr");
+    });
+});
+
+//SITTER-QUIZ//
+let dropInBtn = document.querySelector("#dropIn-btn");
+dropInBtn.addEventListener("click", () =>{
+    let inputDrop = document.querySelector("#dropIn");
+    inputDrop.checked = true;
+});
